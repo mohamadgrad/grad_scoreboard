8bea06625e180f9172a96454a65d82ad5da52a1a
mohamadgrad <113845801+mohamadgrad@users.noreply.github.com>
2022-09-20 18:38:41 +0430

    Add files via upload

---
+++ grad_scoreboard/server/main.lua
@@ -0,0 +1,22 @@
+ -- Question Or Bug = Mohammadgrad#1915
+-- Server Discord : https://discord.gg/zTRDVtmWCa
+
+
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+ESX.RegisterServerCallback('PX_scoreboard:getInfo', function(source, cb)
+	local data = {}
+	local total = #GetPlayers()
+	local xPlayers = ESX.GetPlayers()
+	local adminha = 0
+	for i=1, #xPlayers, 1 do
+		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+		if tonumber(xPlayer.permission_level) >= 1 then
+			adminha = adminha + 1
+		end
+	end
+	data.admins = adminha
+	data.players = total
+	cb(data)
+end)
